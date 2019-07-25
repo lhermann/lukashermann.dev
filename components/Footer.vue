bf110a1ba22091920862f73117e762e811ecedcb
Lukas Hermann <lukher6@web.de>
2019-07-25 10:30:22 +0200

    Add footer

---
+++ components/Footer.vue
@@ -0,0 +1,6 @@
+<template functional>
+  <footer class="wrapper flex py-6 text-gray-600">
+    <strong>&copy; Lukas Hermann</strong> &middot; tentmaker.dev OÜ &middot;
+    Sepapaja 6 &middot; Tallinn 15551 &middot; Estonia
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="pb-16">
+  <div>
     <site-header class="mb-16" />
     <nuxt />
+    <hr class="border-t border-gray-300 mt-32 mb-0" />
+    <site-footer />
   </div>
 </template>
 
 <script>
 import SiteHeader from '~/components/Header'
+import SiteFooter from '~/components/Footer'
 
 export default {
-  components: { SiteHeader }
+  components: { SiteHeader, SiteFooter }
 }
 </script>
