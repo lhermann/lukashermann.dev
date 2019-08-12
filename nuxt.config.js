2aeed70bcaa71ae722ca65154e0e0626a6a69c99
Lukas Hermann <lukher6@web.de>
2019-08-12 20:39:34 +0200

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,9 +19,32 @@ export default {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
-      }
+      },
+      { name: 'msapplication-TileColor', content: '#e88823' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#e88823' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v=2' }
+    ],
     htmlAttrs: {
       lang: 'en'
     }
