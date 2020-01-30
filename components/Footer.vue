44389759990f93f73d2ce281418656e2eb71b995
Lukas Hermann <lukher6@web.de>
2020-01-30 21:31:46 +0100

    feat: add vat id

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,9 @@
 <template functional>
   <footer class="wrapper flex py-6 text-gray-700">
     <p>
-      <strong>&copy; Lukas Hermann</strong> &middot; tentmaker.dev OÜ &middot;
-      Sepapaja 6 &middot; Tallinn 15551 &middot; Estonia
+      <strong>&copy; Lukas Hermann</strong>
+      &middot; tentmaker.dev OÜ &middot; Sepapaja 6 &middot; Tallinn 15551
+      &middot; Estonia &middot; VAT ID: EE102199259
     </p>
   </footer>
 </template>
