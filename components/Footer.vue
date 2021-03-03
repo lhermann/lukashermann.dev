3cce85b6ea3b3ac63c3c9682639cbfbe48ecd0aa
Lukas Hermann <lukher6@web.de>
2021-03-03 10:02:01 +0100

    feat: update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,8 +2,8 @@
   <footer class="wrapper py-6 text-gray-700">
     <p class="text-center">
       <strong>&copy; Lukas Hermann</strong>
-      &middot; tentmaker.dev OÜ &middot; Sepapaja 6 &middot; Tallinn 15551
-      &middot; Estonia &middot; VAT ID: EE102199259
+      &middot; Brenzstrasse 6 &middot; 74405 Gaildorf
+      &middot; Germany
     </p>
     <p class="text-center mt-2">
       Powered by <a href="https://nuxtjs.org">Nuxt</a>, <a href="https://vuejs.org">Vue.js</a>, <a href="https://tailwindcss.com">Tailwindcss</a> and <a href="https://www.netlify.com">Netlify</a>
