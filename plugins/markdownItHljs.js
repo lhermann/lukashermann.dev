ac8959e3e73d72713deda7475fd67e603d667cbd
Lukas Hermann <lukher6@web.de>
2021-03-05 17:03:28 +0100

    feat: improve line number rendering

--- plugins/markdownItHljs.js
+++ plugins/markdownItHljs.js
@@ -1,19 +1,14 @@
 import markdownIt from 'markdown-it'
 import hljs from 'highlight.js'
-import _padStart from 'lodash/padStart'
 import _last from 'lodash/last'
 
 hljs.addPlugin({
   'after:highlight': (result) => {
     const lines = result.value.split('\n')
     if (!_last(lines)) lines.pop() // if last line is empty
-    const genNumber = (i) => {
-      const length = String(lines.length).length
-      return _padStart(String(i), length)
-    }
     for (let i = 0; i < lines.length; i++) {
-      if (lines[i].startsWith('<span class="hljs-line-number">')) continue
-      lines[i] = `<span class="hljs-line-number">${genNumber(i + 1)}</span>` + lines[i]
+      if (lines[i].startsWith('<span class="hljs-line-number"')) continue
+      lines[i] = `<span class="hljs-line-number" data-line-number="${i + 1}"></span>` + lines[i]
     }
     result.value = lines.join('\n')
   },
